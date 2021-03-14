375421bf8a24d0268605d887425a5c5f5c15d016
JonathanGFletcher <jonny.fletcher@icloud.com>
2021-03-14 17:11:02 -0600

    Add pagenation to results page

--- js/main.js
+++ js/main.js
@@ -2,15 +2,42 @@ const app = Vue.createApp({
     data() {
         return {
             keyword: '',
-            result: null
+            result: null,
+            startIndex: 0,
+            prevEnabled: false,
+            nextEnabled: true,
+            booksFound: null
         }
     },
     methods: {
 
         searchGoogleBooks() {
-            fetch('https://www.googleapis.com/books/v1/volumes?q=' + this.keyword + "&startIndex=0&maxResults=20")
+            fetch('https://www.googleapis.com/books/v1/volumes?q=' + this.keyword + "&startIndex=" + this.startIndex + "&maxResults=20")
             .then(res => res.json())
-            .then(json => this.result = json)
+            .then(json => {
+                this.result = json
+
+                if (this.startIndex == 0) {
+                    this.booksFound = this.result.totalItems;
+                    console.log("Result total items: " + this.booksFound);
+                }
+
+                console.log("Start index: " + this.startIndex);
+                if (this.startIndex == 0) { this.prevEnabled = false; }
+                else if (this.startIndex != 0) { this.prevEnabled = true; }
+                
+                if (this.booksFound - this.startIndex < 20) { this.nextEnabled = false; }
+                else { this.nextEnabled = true; }
+            });
+        },
+
+        resetStartIndex() {
+            this.startIndex = 0;
+        },
+
+        resetResults() {
+            this.startIndex = 0;
+            this.result = null;
         }
     }
 });
